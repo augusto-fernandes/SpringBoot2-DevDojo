3d7258beaff9aff2dbb61461a986a1b702f07d36
Augusto Fernandes <augusto.fcosta1808@gmail.com>
2022-03-02 20:56:54 -0300

    Aula 13 - Spring Data JPA pt 01

--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -2,10 +2,21 @@ package academy.devdojo.springboot2.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data // gera os getters/setters equals e etc
 @AllArgsConstructor // cria os construtores
+@NoArgsConstructor // cria um construtor sem argumentos no padrao JPA
+@Entity
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
